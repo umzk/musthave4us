45c562c751f20abfaa633434387d5a0fa985a662
umzk <dedo99999@gmail.com>
2014-09-20 19:33:25 +0700

    convert html to haml

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
 
+    protect_from_forgery with: :exception
+  
   def render_403
     render file: "public/403.html", status: 403
   end
@@ -8,6 +10,6 @@ class ApplicationController < ActionController::Base
     render file: "public/404.html", status: 404
   end
 
-  protect_from_forgery with: :exception
+
 
 end
